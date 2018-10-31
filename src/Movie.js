c783ddf51286b1d4d6cbb60cc4ec02061b203f67
Dodam Shin <dshin20@my.centennialcollege.ca>
2018-10-31 18:08:04 -0400

    initial Commit

--- src/Movie.js
+++ src/Movie.js
@@ -2,47 +2,32 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import './movie.css';
 
-class Movie extends Component{
+function Movie({title,poster}){
+return(
+<div className="">
+<MoviePoster poster={poster} />
+<h2>{title}</h2>
+</div>
+)
+}
 
-
-   static propTypes={
-
+function MoviePoster({poster}){
+return(
+<img src={poster} alt="Movie Poster"/>
+)
+}
+    Movie.propTypes={
     title:PropTypes.string.isRequired,
-    poster:PropTypes.string
+    poster:PropTypes.string.isRequired,
+    genres:PropTypes.array.isRequired,
+    synopsis:PropTypes.string.isRequied
+   }
+
+   MoviePoster.PropTypes={
+       poster:PropTypes.string.isRequired
    }
 
 
-
-
-
-
-
-
-
-
-
-    render(){
-        console.log(this.props);
-        return(
-
-        <div>      
-           <MoviePoster poster={this.props.poster} /> 
-           <h1>{this.props.title}</h1>
-        </div>
-        )
-
-       
-    }
-}
-
-class MoviePoster extends Component{
-  render(){
-      return(
-          // eslint-disable-next-line
-          <img src={this.props.poster} />
-      )
-  }
-}
 
 
 
