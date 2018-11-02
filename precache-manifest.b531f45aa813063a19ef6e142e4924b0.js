72a2517d0c885517442e8fcd29592c80bb1497c4
Dodam Shin <dshin20@my.centennialcollege.ca>
2018-11-02 12:04:47 -0400

    Updates

---
+++ precache-manifest.b531f45aa813063a19ef6e142e4924b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0929535379473f138023",
+    "url": "/REACT-MOVIEAPP/static/js/runtime~main.09295353.js"
+  },
+  {
+    "revision": "ef4c4c3e0a885ab85e83",
+    "url": "/REACT-MOVIEAPP/static/js/main.ef4c4c3e.chunk.js"
+  },
+  {
+    "revision": "0615ddca4c939a87eed7",
+    "url": "/REACT-MOVIEAPP/static/js/1.0615ddca.chunk.js"
+  },
+  {
+    "revision": "ef4c4c3e0a885ab85e83",
+    "url": "/REACT-MOVIEAPP/static/css/main.0c9ee596.chunk.css"
+  },
+  {
+    "revision": "3a59994753d93cc7c6475cb903c367f1",
+    "url": "/REACT-MOVIEAPP/index.html"
+  }
+];
